ab1750526da116ea87731a2a997959dccf69a6d3
Alan-Kuan <kuanhaoan@gmail.com>
2025-04-26 19:07:07 +0800

    feat(shell): add pwd

--- 3_shell/src/parser.rs
+++ 3_shell/src/parser.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 // @return: whether to exit
 pub fn parse_cmd_line(cmd_line: &str) -> bool {
     let args: Vec<&str> = cmd_line.trim().split(' ').collect();
@@ -6,6 +8,7 @@ pub fn parse_cmd_line(cmd_line: &str) -> bool {
         "exit" => return true,
         "echo" => echo(args),
         "cd" => cd(args),
+        "pwd" => pwd(args),
         _ => exec(args),
     }
     false
@@ -23,6 +26,17 @@ fn echo(args: Vec<&str>) {
 
 fn cd(args: Vec<&str>) {
     // TODO
+}
+
+fn pwd(args: Vec<&str>) {
+    if args.len() > 1 {
+        println!("pwd: too many arguments");
+        return;
+    }
+    match env::current_dir() {
+        Ok(path) => println!("{}", path.display()),
+        Err(_) => println!("pwd: failed to get current directory"),
+    }
 }
 
 fn exec(args: Vec<&str>) {
