46e5d87e565a5583edba645984a06b9d3e01139c
Alan-Kuan <kuanhaoan@gmail.com>
2025-05-03 10:40:56 +0800

    feat(shell): handle quotes

--- 3_shell/src/parser.rs
+++ 3_shell/src/parser.rs
@@ -1,28 +1,78 @@
 use std::env;
 use std::path::PathBuf;
 use std::process::Command;
-use std::str::Split;
+
+enum ParsingState {
+    Normal,
+    InsideSingleQuote,
+    InsideDoubleQuote,
+}
 
 /// # Returns
 ///
 /// whether to exit
 pub fn parse_cmd_line(cmd_line: &str) -> bool {
-    let mut args = cmd_line.trim().split(' ');
-    let cmd = args.next().unwrap();
+    let mut args: Vec<String> = vec![];
+    let mut arg = String::new();
+    let mut state = ParsingState::Normal;
 
-    match cmd {
+    for ch in cmd_line.chars() {
+        match ch {
+            ' ' => {
+                match state {
+                    ParsingState::Normal => {
+                        if !arg.is_empty() {
+                            args.push(arg.clone());
+                        }
+                        arg.clear();
+                    },
+                    _ => arg.push(ch),
+                }
+            },
+            '\'' => {
+                match state {
+                    ParsingState::Normal => state = ParsingState::InsideSingleQuote,
+                    ParsingState::InsideSingleQuote => state = ParsingState::Normal,
+                    _ => arg.push(ch),
+                }
+            },
+            '"' => {
+                match state {
+                    ParsingState::Normal => state = ParsingState::InsideDoubleQuote,
+                    ParsingState::InsideDoubleQuote => state = ParsingState::Normal,
+                    _ => arg.push(ch),
+                }
+            },
+            '\n' => {
+                match state {
+                    ParsingState::Normal => {
+                        if !arg.is_empty() {
+                            args.push(arg.clone());
+                        }
+                    },
+                    _ => {
+                        eprintln!("Unclosed quotes");
+                        return false;
+                    }
+                }
+            },
+            _ => arg.push(ch),
+        }
+    }
+
+    match args[0].as_str() {
         "" => (),
         "exit" => return true,
         "echo" => echo(args),
         "cd" => cd(args),
         "pwd" => pwd(args),
-        _ => exec(cmd, args),
+        _ => exec(args),
     }
     false
 }
 
-fn echo(args: Split<'_, char>) {
-    for arg in args {
+fn echo(args: Vec<String>) {
+    for arg in &args[1..] {
         if arg.is_empty() {
             continue;
         }
@@ -31,14 +81,13 @@ fn echo(args: Split<'_, char>) {
     println!();
 }
 
-fn cd(args: Split<'_, char>) {
-    let args: Vec<&str> = args.collect();
+fn cd(args: Vec<String>) {
     let path;
 
     match args.len() {
         #![allow(deprecated)]
-        0 => path = env::home_dir().unwrap(),
-        1 => path = PathBuf::from(args[0]),
+        1 => path = env::home_dir().unwrap(),
+        2 => path = PathBuf::from(&args[1]),
         _ => {
             eprintln!("cd: too many arguments");
             return;
@@ -50,8 +99,8 @@ fn cd(args: Split<'_, char>) {
     }
 }
 
-fn pwd(args: Split<'_, char>) {
-    if args.count() > 1 {
+fn pwd(args: Vec<String>) {
+    if args.len() > 1 {
         eprintln!("pwd: too many arguments");
         return;
     }
@@ -61,8 +110,8 @@ fn pwd(args: Split<'_, char>) {
     }
 }
 
-fn exec(cmd: &str, args: Split<'_, char>) {
-    match Command::new(cmd).args(args).status() {
+fn exec(args: Vec<String>) {
+    match Command::new(&args[0]).args(&args[1..]).status() {
         Ok(_) => (),
         Err(err) => eprintln!("exec: {}", err),
     }
