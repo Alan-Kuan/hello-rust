a80a0eedb10db707771a47124859476d3d6f7bc5
Alan-Kuan <kuanhaoan@gmail.com>
2025-05-10 16:33:40 +0800

    chore(shell): update messages

--- 3_shell/src/main.rs
+++ 3_shell/src/main.rs
@@ -8,11 +8,11 @@ pub mod parser;
 fn main() {
     loop {
         print!("> ");
-        io::stdout().flush().expect("Failed to flush.");
+        io::stdout().flush().expect("shell: failed to flush");
 
         let mut line = String::new();
         let bytes_read = io::stdin().read_line(&mut line)
-            .expect("Failed to read line.");
+            .expect("shell: failed to read line");
 
         if bytes_read == 0 || parse_cmd_line(&line) {
             break;
